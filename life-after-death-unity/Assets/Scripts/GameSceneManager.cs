98977a8e49fe2b14fd60e0186b48dabb6060ea6e
Martin Forget <martforg@hotmail.com>
2018-11-02 23:59:16 -0400

    - Added state change event on GameSceneManager.

--- life-after-death-unity/Assets/Scripts/GameSceneManager.cs
+++ life-after-death-unity/Assets/Scripts/GameSceneManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameSceneManager : MonoBehaviour
 {
     static GameSceneManager instance = null;
+
+    static public event Action<PlayerStats.PlayerState> onPlayerStateChange;
+
+    PlayerStats.PlayerState playerState = PlayerStats.PlayerState.Alive;
 
     [SerializeField]
     Player player;
@@ -20,16 +25,27 @@ public class GameSceneManager : MonoBehaviour
     void Start()
     {
         followCamera.Target = player.transform;
+        playerState = player.PlayerStats.State;
+        player.PlayerStats.onPlayerStateChange += OnStateChangeHandler;
     }
 
     void OnDestroy()
     {
         if (instance == this)
             instance = null;
+
+        if (player != null && player.PlayerStats != null)
+            player.PlayerStats.onPlayerStateChange -= OnStateChangeHandler;
     }
 
     static public Player ActivePlayer
     {
         get { return instance.player; }
+    }
+
+    void OnStateChangeHandler(PlayerStats.PlayerState newState)
+    {
+        playerState = newState;
+        onPlayerStateChange(playerState);
     }
 }
