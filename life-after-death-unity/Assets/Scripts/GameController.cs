d7d4e2f0c68fa88b07f4caaff720fea63e38ceb9
Martin Forget <martforg@hotmail.com>
2018-11-02 21:15:38 -0400

    - Added GameController.

---
+++ life-after-death-unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static GameController instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }else
+        {
+            Destroy(this);
+        }
+    }
+}
