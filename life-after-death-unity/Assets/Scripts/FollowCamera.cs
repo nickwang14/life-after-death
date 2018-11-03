516e441fde73342c8ad04d5088c087b6064827e9
Martin Forget <martforg@hotmail.com>
2018-11-02 22:00:38 -0400

    - Added FollowCamera script. - Added FollowCamera prefab.

---
+++ life-after-death-unity/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    float followSpeed;
+
+    [SerializeField]
+    Camera followCamera;
+
+    Transform followTarget = null;
+    bool shouldFollow = true;
+
+    public bool Follow
+    {
+        get { return shouldFollow; }
+        set { shouldFollow = true; }
+    }
+
+    public Transform Target
+    {
+        get { return followTarget; }
+        set { followTarget = value; }
+    }
+
+    void Update()
+    {
+        if (followTarget != null && shouldFollow)
+            FollowTarget(Time.deltaTime);
+    }
+
+    void FollowTarget(float deltaTime)
+    {
+        Vector3 newPosition = Vector2.MoveTowards(transform.position, followTarget.position, deltaTime * followSpeed);
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+}
---
+++ life-after-death-unity/Assets/Scripts/MartinTestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartinTestScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    FollowCamera followCam;
+
+    void Start()
+    {
+        followCam.Target = target;
+    }
+}
