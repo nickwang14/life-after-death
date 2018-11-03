6d58d60959d40aa692dab271a31da4bc46cff54f
martforg <martforg@notarealemail.com>
2018-11-03 11:29:04 -0400

    - Added script to handle progression flags.

---
+++ life-after-death-unity/Assets/Scripts/ProgresionFlags.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ProgresionFlags
+{
+    public event Action<string> onFlagAdded = delegate { };
+
+    HashSet<string> flagHashSet = new HashSet<string>();
+
+    public void AddFlag(string flagName)
+    {
+        if (!HasFlag(flagName))
+        {
+            flagHashSet.Add(flagName);
+            onFlagAdded(flagName);
+        }
+    }
+
+    public bool HasFlag(string flagName)
+    {
+        return flagHashSet.Contains(flagName);
+    }
+}
