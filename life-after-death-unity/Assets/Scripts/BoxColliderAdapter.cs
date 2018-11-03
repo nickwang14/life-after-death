aec5e72cab82fd96571c35fae23e2ce3b3cf68a0
martforg <martforg@notarealemail.com>
2018-11-03 12:16:41 -0400

    - Added script to adapt collider size to wall and floor.

---
+++ life-after-death-unity/Assets/Scripts/BoxColliderAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxColliderAdapter : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    BoxCollider2D boxCollider;
+
+    public void AdaptColldier()
+    {
+        Vector2 size = spriteRenderer.size;
+        boxCollider.size = size;
+    }
+}
---
+++ life-after-death-unity/Assets/Scripts/Editor/BoxColliderAdapterEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BoxColliderAdapter))]
+public class BoxColliderAdapterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        BoxColliderAdapter boxColliderAdapter = target as BoxColliderAdapter;
+
+        if (GUILayout.Button("Adapt collider"))
+        {
+            boxColliderAdapter.AdaptColldier();
+        }
+    }
+}
