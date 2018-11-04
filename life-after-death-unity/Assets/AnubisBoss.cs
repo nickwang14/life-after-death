b92e9f017544a300adcd6256a5ca184396d62d18
Kyle Purdy <kylepurdy@rogers.com>
2018-11-04 15:15:18 -0500

    Anubis Boss

---
+++ life-after-death-unity/Assets/AnubisBoss.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnubisBoss : MonoBehaviour {
+
+    [SerializeField]
+    GameObject projecileSprite;
+
+    List<GameObject> ProjectilePool;
+
+    [SerializeField]
+    float TimerToFire = 2.0f;
+
+    [SerializeField]
+    float bulletSpeed = 1.0f;
+
+    [SerializeField]
+    int spreadBulletAmount = 10;
+
+    float fireParticleTimer;
+
+    [SerializeField]
+    GameObject FireLocation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ProjectilePool = new List<GameObject>();
+        for(int i = 0; i < 300; i++)
+        {
+            ProjectilePool.Add(Instantiate(projecileSprite, FireLocation.transform.position, Quaternion.Euler(0,0,-90)));
+            ProjectilePool[i].SetActive(false);
+        }
+
+        fireParticleTimer = TimerToFire;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        fireParticleTimer -= 1.0f * Time.deltaTime;
+
+        if(fireParticleTimer <= 0)
+        {
+            int attackChoice = Random.Range(0, 100);
+
+            if (attackChoice >= 0 && attackChoice <= 50)
+                Fire();
+
+            else if (attackChoice > 50 && attackChoice <= 60)
+                FireSpread(spreadBulletAmount);
+
+            else
+            {
+                SpawnSkeleton();
+            }
+
+            fireParticleTimer = TimerToFire;
+        }
+
+    }
+
+    void Fire()
+    {
+        GameObject tempProjectile;
+
+        for(int i = 0; i < 300; i++)
+        {
+            if(!ProjectilePool[i].activeInHierarchy)
+            {
+                tempProjectile = ProjectilePool[i];
+                tempProjectile.transform.position = FireLocation.transform.position;
+                tempProjectile.transform.rotation = Quaternion.Euler(0, 0, -90);
+
+                float randomRotation = Random.Range(-30, 30);
+                tempProjectile.transform.Rotate(new Vector3(0, 0, randomRotation));
+                tempProjectile.SetActive(true);
+                tempProjectile.GetComponent<Bullet>().Fire(bulletSpeed);
+                break;
+            }
+        }
+    }
+
+    void FireSpread(int amountOfBullets)
+    {
+        float degreeIncrement = 170 / amountOfBullets;
+
+        GameObject tempProjectile;
+
+        for (int a = 0; a < amountOfBullets; a++)
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                if (!ProjectilePool[i].activeInHierarchy)
+                {
+                    tempProjectile = ProjectilePool[i];
+                    tempProjectile.transform.position = FireLocation.transform.position;
+                    tempProjectile.transform.rotation = Quaternion.Euler(0, 0, -90);
+                    tempProjectile.transform.Rotate(new Vector3(0, 0, -90 + (degreeIncrement * a)));
+                    tempProjectile.SetActive(true);
+                    tempProjectile.GetComponent<Bullet>().Fire(bulletSpeed);
+                    break;
+                }
+            }
+        }
+    }
+
+    void SpawnSkeleton()
+    {
+
+    }
+}
---
+++ life-after-death-unity/Assets/Bullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float Speed;
+
+    float bulletLifeTime = 10.0f;
+    float maxLifeTime = 10.0f;
+
+    [SerializeField]
+    int DamageAmount = 10;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += new Vector3(Speed * transform.up.x, Speed * transform.up.y, 0) * Time.deltaTime;
+
+        bulletLifeTime -= 1.0f * Time.deltaTime;
+        if (bulletLifeTime <= 0)
+            Disable();
+
+    }
+
+    public void Fire(float speed)
+    {
+        Speed = speed;
+    }
+
+    void Disable()
+    {
+        bulletLifeTime = maxLifeTime;
+        this.gameObject.SetActive(false);
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+       
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.PlayerStats.DamagePlayer(DamageAmount);
+            Disable();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Player player = col.gameObject.GetComponent<Player>();
+        if (player == null)
+        {
+            Disable();
+        }
+    }
+}
