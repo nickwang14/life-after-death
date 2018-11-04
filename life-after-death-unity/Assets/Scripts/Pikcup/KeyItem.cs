4eae815b3be514a90763df8334b21efb40dd30ff
martforg <martforg@notarealemail.com>
2018-11-03 21:06:22 -0400

    - Added scripts for item pickup.

---
+++ life-after-death-unity/Assets/Scripts/Pikcup/KeyItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : PickupItem
+{
+    protected override void PickUpItem(Player player)
+    {
+        player.PlayerStats.AddKeys(1);
+    }
+}
