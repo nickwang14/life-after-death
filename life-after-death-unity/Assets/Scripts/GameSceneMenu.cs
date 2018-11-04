b22736e635a507d932f6e85a4da760632d055205
martforg <martforg@notarealemail.com>
2018-11-03 22:56:41 -0400

    - Added menu in the game scene.

---
+++ life-after-death-unity/Assets/Scripts/GameSceneMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneMenu : MonoBehaviour
+{
+    public void OpenMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ContinueButtonClick()
+    {
+        CloseMenu();
+        GameSceneManager.UnPauseGame();
+    }
+
+    public void RestartAtCheckpointButtonClick()
+    {
+        //Implement checkpoints
+        GameSceneManager.UnPauseGame();
+    }
+
+    public void QuitButtonClick()
+    {
+        GameSceneManager.UnPauseGame();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
