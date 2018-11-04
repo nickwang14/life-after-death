dc44a48beef74fe4b63b5986cf562b16892bfdb6
martforg <martforg@notarealemail.com>
2018-11-03 20:15:27 -0400

    - Fixed damage handling when dead.

--- life-after-death-unity/Assets/Scripts/Enemy/EnemyHitBox.cs
+++ life-after-death-unity/Assets/Scripts/Enemy/EnemyHitBox.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EnemyHitBox : MonoBehaviour
 {
     [SerializeField]
-    float damageAmout;
+    float damageAmount;
 
     void OnTriggerStay2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
         if (player != null)
-            player.PlayerStats.DamagePlayer(damageAmout);
+            player.PlayerStats.DamagePlayer(damageAmount);
     }
 }
